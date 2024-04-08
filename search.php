0923ba372aeb27daaecdf90cedaf83bf86863bf1
MaggieD20 <e2160601@cmaisonneuve.qc.ca>
2024-04-08 10:45:42 -0400

    search page

---
+++ search.php
@@ -0,0 +1,39 @@
+<?php
+// modele
+?>
+
+<?php get_header(); ?>
+<h2>search.php</h2>
+
+
+<div id="accueil" class="global">
+    <section class="accueil__section">
+        <h2>Résultat de la recherche</h2>
+        <div class="flexbox">
+
+            <?php if (have_posts()) :
+                while (have_posts()) : the_post(); ?>
+
+
+                    <div class="carte">
+
+                        <?php the_title();
+                        ?>
+                        <?php
+                        echo wp_trim_words(get_the_content(), 10); ?>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif;  ?>
+        </div>
+    </section>
+</div>
+<!-- ---------footer---------- -->
+<div id="footer" class="global">
+    <footer class="footer__section">
+        <h2>footer</h2>
+        <button>merci</button>
+    </footer>
+</div>
+</body>
+
+</html>
