55b99c68da2b6ccd3236c9dd7249e07c4891fe48
MaggieD20 <e2160601@cmaisonneuve.qc.ca>
2024-04-08 19:43:27 -0400

    les cartes de la page de recherche

--- search.php
+++ search.php
@@ -7,7 +7,7 @@
 <div id="accueil" class="global">
     <section class="accueil__section">
         <h2>Résultat de la recherche</h2>
-        <div class="flexbox">
+        <div class="section__ville">
 
             <?php if (have_posts()) :
                 while (have_posts()) : the_post(); ?>
@@ -15,10 +15,8 @@
 
                     <div class="carte">
 
-                        <?php the_title();
-                        ?>
-                        <?php
-                        echo wp_trim_words(get_the_content(), 10); ?>
+                       <h2><?php the_title(); ?></h2> 
+                       <p> <?php echo wp_trim_words(get_the_content(), 10); ?></p>
                     </div>
                 <?php endwhile; ?>
             <?php endif;  ?>
