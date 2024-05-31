6174e37ea9125337a9b5e4811c860b2c3e9ca944
MaggieD20 <coucoumaggie@hotmail.com>
2024-05-31 15:43:46 -0400

    short_code

--- template-pays.php
+++ template-pays.php
@@ -5,20 +5,21 @@
  *
  */
 ?>
-
 <?php get_header(); ?>
+
 <main class="intro__pays">
+    <?php get_template_part("gabarit/vague"); ?>
     <?php
     if (have_posts()) : the_post(); ?>
         <h1><?= get_the_title(); ?></h1>
         <?php the_content(); ?>
     <?php endif; ?>
-    <?php get_template_part("gabarit/vague"); ?>
+
 </main>
 <div class="rest_pays">
-    <p>hgxrtxgf
-    </p>
-
+    <p>hgxrtxgf </p>
+    <?php echo do_shortcode("[pays]"); ?>
+    <?php get_template_part("gabarit/vague"); ?>
 </div>
 <?php
 get_footer();
