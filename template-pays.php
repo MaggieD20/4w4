b57a2b60e5fa61deb1a396cab939471beeb4326f
MaggieD20 <coucoumaggie@hotmail.com>
2024-05-31 16:14:01 -0400

    ef-md vague positionnement et couleur

--- template-pays.php
+++ template-pays.php
@@ -8,17 +8,19 @@
 <?php get_header(); ?>
 
 <main class="intro__pays">
+    <div>
+        <?php
+        if (have_posts()) : the_post(); ?>
+            <h1><?= get_the_title(); ?></h1>
+            <?php the_content(); ?>
+        <?php endif; ?>
+    </div>
     <?php get_template_part("gabarit/vague"); ?>
-    <?php
-    if (have_posts()) : the_post(); ?>
-        <h1><?= get_the_title(); ?></h1>
-        <?php the_content(); ?>
-    <?php endif; ?>
-
 </main>
 <div class="rest_pays">
-    <p>hgxrtxgf </p>
-    <?php echo do_shortcode("[pays]"); ?>
+    <div class="api">
+        <?php echo do_shortcode("[pays]"); ?>
+    </div>
     <?php get_template_part("gabarit/vague"); ?>
 </div>
 <?php
