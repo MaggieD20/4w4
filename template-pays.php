6614c3a2e55f023ff4108db4361b08cc82b54fec
MaggieD20 <coucoumaggie@hotmail.com>
2024-05-31 14:08:16 -0400

    ajout du post

--- template-pays.php
+++ template-pays.php
@@ -7,6 +7,12 @@
 ?>
 
 <?php get_header(); ?>
-
+<main class="site__main">
+    <?php
+    if (have_posts()) : the_post(); ?>
+        <h1><?= get_the_title(); ?></h1>
+        <?php the_content(); ?>
+    <?php endif; ?>
+</main><!-- #main -->
 <?php
 get_footer();
